bf3fcf793f3f5a670c7f921b4ebd091b8a2767f0
VikaZhou <my12time@gmail.com>
2016-06-27 18:35:52 +0800

    fix error

--- taobaok/commands/add_product.py
+++ taobaok/commands/add_product.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import re
 from scrapy.commands import ScrapyCommand
 from scrapy.exceptions import UsageError
@@ -6,34 +8,33 @@ import pymongo
 
 class Command(ScrapyCommand):
 
-	requires_project = True
-	default_settings = {'LOG_ENABLED': True}
+    requires_project = True
+    default_settings = {'LOG_ENABLED': True}
 
-	def syntax(self):
-		return '<num_id>'
+    def syntax(self):
+        return '<num_id>'
 
-	def short_desc(self):
-		return 'Add Taobao Item'
+    def short_desc(self):
+        return 'Add Taobao Item'
 
-	def long_desc(self):
-		return 'Add a new Taobao Item <num_id> to the Crawler'
+    def long_desc(self):
+        return 'Add a new Taobao Item <num_id> to the Crawler'
 
-	def _err(self, msg):
+    def _err(self, msg):
         sys.stderr.write(msg + os.linesep)
         self.exitcode = 1
 
-	def run(self, args, opts):
-		if len(args) != 1 or not re.match('[0-9]+', args[0]):
+    def run(self, args, opts):
+        if len(args) != 1 or not re.match('[0-9]+', args[0]):
             raise UsageError()
 
          client = pymongo.MongoClient(self.settings['MONGO_URI'])
          db = self.client[self.settings['MONGO_DB']]
          doc = {
-         	'num_id': args[0],
-         	'detail_url': 'https://item.taobao.com/item.htm?id=%s' % args[0]
-         	'status': 'NEW',
+             'num_id': args[0],
+             'detail_url': 'https://item.taobao.com/item.htm?id=%s' % args[0]
+             'status': 'NEW',
          }
          db['crawl_uris'].insert_one(doc)
          client.close()
          
-
